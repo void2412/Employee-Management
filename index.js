b0a749781d82114a8fe381e2a23306a123082ddf
void2412 <void2412@gmail.com>
2022-06-02 17:05:24 +0930

    added structure functions for index

---
+++ index.js
@@ -0,0 +1,34 @@
+const cTable = require('console.table')
+const sqlQuery = require('./sqlQuery')
+
+async function mainMenu(){
+
+}
+
+async function viewAllDepartments(){
+
+}
+
+async function viewAllRoles(){
+
+}
+
+async function viewAllEmployees(){
+
+}
+
+async function addDepartment(name){
+
+}
+
+async function addRole(name, salary, departmentId){
+
+}
+
+async function addEmployee(firstName, lastName, roleId, managerId){
+
+}
+
+async function updateRole(employeeId, roleId){
+
+}
--- sqlQuery.js
+++ sqlQuery.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql2')
-const cTable = require('console.table')
+
 require('dotenv').config()
 class sqlQuery {
 	constructor(){
