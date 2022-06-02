b3cd43ffe50681e9dfae396fd91c29152b026466
void2412 <void2412@gmail.com>
2022-06-02 16:20:16 +0930

    added coded for all views

--- sqlQuery.js
+++ sqlQuery.js
@@ -1,5 +1,6 @@
-const mysql = require('mysql2');
-
+const mysql = require('mysql2')
+const cTable = require('console.table')
+require('dotenv').config()
 class sqlQuery {
 	constructor(){
 		this.host = process.env.host
@@ -19,15 +20,19 @@ class sqlQuery {
 	}
 
 	viewAllDepartments(){
-
+		this.connection.query(`select * from department`, (err, result) => err ? console.log(err): console.table(result))
 	}
 
 	viewAllRoles(){
-
+		this.connection.query(`select id, title as name from role`, (err, result) => err ? console.log(err): console.table(result))
 	}
 
 	viewAllEmployees(){
-
+		this.connection.query(`select t1.id, t1.first_name, t1.last_name, role.title, department.name, concat(t2.first_name,' ', t2.last_name) as manager from employee t1
+		join role on t1.role_id = role.id
+		join department on role.department_id = department.id
+		left join employee t2 on t1.manager_id = t2.id
+		order by t1.id`, (err, result) => err ? console.log(err): console.table(result))
 	}
 
 	addDepartment(){
@@ -46,7 +51,6 @@ class sqlQuery {
 		
 	}
 }
-
 
 
 module.exports = sqlQuery
