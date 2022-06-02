3909b9453d60108f1dc263724140f95931e3ac91
void2412 <void2412@gmail.com>
2022-06-02 17:15:33 +0930

    added logic for mainMenu function

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const cTable = require('console.table')
 const sqlQuery = require('./sqlQuery')
+const inquirer = require('inquirer')
+
+const menuList = [
+	'View All Departments', 
+	'View All Roles', 
+	'View All Employees',
+	'Add Department',
+	'Add Role',
+	'Add Employee',
+	'Update Role For Employee',
+	'Quit'
+	]
 
 async function mainMenu(){
+	while(true){
+		var userChoice = await inquirer.prompt({
+			type:'list',
+			name: 'choice',
+			message: 'What do you want to do?',
+			choices: menuList
+		})
 
+		switch(userChoice.choice){
+			case menuList[0]: await viewAllDepartments(); break;
+			case menuList[1]: await viewAllRoles(); break;
+			case menuList[2]: await viewAllEmployees(); break;
+			case menuList[3]: await addDepartment(); break;
+			case menuList[4]: await addRole(); break;
+			case menuList[5]: await addEmployee(); break;
+			case menuList[6]: await updateRole(); break;
+			case menuList[7]: return;
+		}
+	}
+	
 }
-
+mainMenu()
 async function viewAllDepartments(){
 
 }
@@ -17,18 +48,18 @@ async function viewAllEmployees(){
 
 }
 
-async function addDepartment(name){
+async function addDepartment(){
 
 }
 
-async function addRole(name, salary, departmentId){
+async function addRole(){
 
 }
 
-async function addEmployee(firstName, lastName, roleId, managerId){
+async function addEmployee(){
 
 }
 
-async function updateRole(employeeId, roleId){
+async function updateRole(){
 
 }
